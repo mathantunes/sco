99e9b1c315f5ba9fc75ff23182e7b9c0450119f4
mathantunes <matheus@jamtech.ch>
2026-08-29 19:02:10 +0200

    get deviceId from query parameters

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,9 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { SessionProvider } from './providers/SessionProvider'
+
+const deviceId = new URLSearchParams(window.location.search).get('deviceId') || 'device1'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <SessionProvider deviceId={deviceId}>
+      <App />
+    </SessionProvider>
   </StrictMode>,
 )
