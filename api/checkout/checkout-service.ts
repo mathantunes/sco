86324d33d3a0414c34bc0957f6e3e710c367ced5
mathantunes <matheus@jamtech.ch>
2026-08-29 18:57:05 +0200

    leave a commented out version of a failure checkout response

--- api/checkout/checkout-service.ts
+++ api/checkout/checkout-service.ts
@@ -6,6 +6,12 @@ export class CheckoutService {
         // Mock implementation of the checkout process
         await new Promise((resolve) => setTimeout(resolve, 5000));
 
+        // return {
+        //     kind: "failure",
+        //     reason: "Payment failed due to insufficient funds.",
+        //     code: 402
+        // }
+        
         return {
             kind: "success",
             transactionId: `txn_${crypto.randomUUID()}`,
