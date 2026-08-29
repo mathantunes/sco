640f256ceb42cf334f770d4b4a1240a182e2fbb0
mathantunes <matheus@jamtech.ch>
2026-08-29 18:00:20 +0200

    domain functions to update order items

--- packages/contracts/order.ts
+++ packages/contracts/order.ts
@@ -30,3 +30,65 @@ export type OrderItem = {
 };
 
 export type OrderProduct = Omit<Product, "imageUrl">;
+
+export const replaceItemInOrder = (order: OpenOrder, orderItem: OrderItem): OpenOrder => {
+    const existingItemIndex = order.items.findIndex(item => item.product.id === orderItem.product.id);
+    if (existingItemIndex !== -1) {
+        const updatedItems = [
+            ...order.items.slice(0, existingItemIndex),
+            orderItem,
+            ...order.items.slice(existingItemIndex + 1)
+        ];
+
+        return {
+            ...order,
+            items: updatedItems
+        };
+    } else {
+        return {
+            ...order,
+            items: [...order.items, orderItem]
+        };
+    }
+}
+
+export const addItemToOrder = (order: OpenOrder, orderItem: OrderItem): OpenOrder => {
+    const existingItemIndex = order.items.findIndex(item => item.product.id === orderItem.product.id);
+    if (existingItemIndex !== -1) {
+        const existingItem = order.items[existingItemIndex];
+        const updatedItem: OrderItem = {
+            ...existingItem,
+            quantity: existingItem.quantity + orderItem.quantity,
+        };
+        const updatedItems = [
+            ...order.items.slice(0, existingItemIndex),
+            updatedItem,
+            ...order.items.slice(existingItemIndex + 1)
+        ];
+
+        return {
+            ...order,
+            items: updatedItems
+        };
+    } else {
+        return {
+            ...order,
+            items: [...order.items, orderItem]
+        };
+    }
+}
+
+export const computeTotalPrice = (order: OpenOrder): OpenOrder => {
+    const totalAmount = order.items.reduce((sum, item) => {
+        const itemTotal = parseFloat(item.product.price.amount) * item.quantity;
+        return sum + itemTotal;
+    }, 0);
+
+    return {
+        ...order,
+        totalPrice: {
+            amount: totalAmount.toFixed(2),
+            currency: "USD"
+        }
+    };
+}
